ad9bb8c368c59bef0664851f6e664ea0a047e50d
wandouni <2539419557@qq.com>
2022-04-04 23:59:37 +0800

    add dist

--- testwebpack/dist-store/es6es6-dist/index.js
+++ testwebpack/dist-store/es6es6-dist/index.js
@@ -1,3 +1,9 @@
+/*
+ * @Author: wandouni
+ * @Date: 2022-04-04 22:46:44
+ * @LastEditors: wandouni
+ * @LastEditTime: 2022-04-04 23:58:41
+ */
 (() => {
   "use strict";
   var r = {
@@ -9,9 +15,12 @@
     },
     o = {};
   (function t(e) {
+    // 判断是否有缓存
     var n = o[e];
     if (void 0 !== n) return n.exports;
+    // 添加缓存，定义空exports对象
     var s = (o[e] = { exports: {} });
+    // 执行
     return r[e](s, s.exports, t), s.exports;
   })(54).foo();
 })();
